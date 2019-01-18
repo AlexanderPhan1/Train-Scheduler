cf0ba96b72762e3736437b235a0c557c2309e142
AlexanderPhan <42187090+AlexanderPhan@users.noreply.github.com>
2019-01-18 15:39:10 -0600

    adjust changes to the site and firebase database

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Train-Scheduler
+Overview
+
+This is a Firebase backed train schedule application. It uses HTML, CSS and jQuery to render the schedule and admin panel on the web page.
+
+Uses Firebase to host a globally available set of data that will be retrieved and manipulated using Momen.js to provide up-to-date information about various trains arrival times and minutes until arrival.
+
+Instructions
+Make sure that your app suits this basic spec:
+
+
+When adding trains, administrators should be able to submit the following:
+Train Name
+Destination 
+First Train Time -- in military time
+Frequency -- in minutes
+Code this app to calculate when the next train will arrive; this should be relative to the current time.
+Users from many different machines must be able to view same train times.
+Styling and theme are completely up to you. Get Creative!
--- content/javascript/app.js
+++ content/javascript/app.js
@@ -1,188 +1,135 @@
-// Initialize Firebase
+
+$(document).ready(function(){
+  
+// 1. Initialize Firebase
 var config = {
     apiKey: "AIzaSyDt1PmYRLAKN2GnyGsXu9wnQ1gVgPXQW_8",
     authDomain: "train-schedule-database-d626d.firebaseio.com/",
     databaseURL: "https://train-schedule-database-d626d.firebaseio.com/",
     projectId: "train-schedule-database-d626d",
-    storageBucket: "gs://train-schedule-database-d626d.appspot.com",
+    storageBucket: "train-schedule-database-d626d.appspot.com",
     messagingSenderId: "579703207440"
-  };
-  firebase.initializeApp(config);
+};
+    firebase.initializeApp(config);
+    
+// ================================================================================================================================
+var database = firebase.database();
+
+
+$("#addTrainBtn").on("click", function(event){
+     
+     event.preventDefault();
+
+	
+	var trainName = $("#trainNameInput").val().trim();
+	var dest = $("#destinationInput").val().trim();
+	var firstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm" ).format("X");
+	var freq = $("#frequencyInput").val().trim();
+	
+
+	
+	var newTrain = {
+		name:  trainName,
+		dest: dest,
+		start: firstTrain,
+		frequency: freq
+	}
+
+	
+	database.ref().push(newTrain);
+	
+	console.log(newTrain.name);
+	console.log(newTrain.dest);
+	console.log(newTrain.start);
+	console.log(newTrain.frequency);
+
+
+	alert("Train Schedule successfully added");
+
+
+	$("#trainNameInput").val("");
+	$("#destinationInput").val("");
+	$("#firstTrainInput").val("");
+	$("#frequencyInput").val("");
+
+
+	return false;
+
+});
+
+
+
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
+
+	console.log(childSnapshot.val());
+
+	
+	var trainName = childSnapshot.val().name;
+	var trainDest = childSnapshot.val().dest;
+	var trainStart = childSnapshot.val().start;
+	var trainFreq = childSnapshot.val().frequency;
+	var key = childSnapshot.key;
+	var remove = "<button class='glyphicon glyphicon-trash' id=" + key + "></button>"
+	var updateMe = "<button class='glyphicon glyphicon-edit' id=" + key + "></button>"
+
+	
+	console.log(trainName);
+	console.log(trainDest);
+	console.log(trainStart);
+	console.log(trainFreq);
+
+
+   var currentTime = moment();
+    console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+    setInterval(function(){
+    $('#current-status').html("Current Time: " + moment().format('dddd, MMMM Do YYYY, HH:mm:ss a'))
+   }, 1000);
+
+
+
+  var firstTimeConverted = moment(trainStart, "hh:mm").subtract(1, "years");
+  //console.log("FTC: "+firstTimeConverted);
+
+ 
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
   
-  var database = firebase.database()
-  var trainDataRef = firebase.database().ref('/train_data')
   
+  var timeRemainder = diffTime % trainFreq ;
+
   
+  var minutes = trainFreq - timeRemainder;
+
   
+  var nextTrain = moment().add(minutes, "minutes");
+
   
+  var nextTrainArrival = moment(nextTrain).format("hh:mm a");
+	
+
   
+  $("#trainTable > tbody").prepend("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + nextTrainArrival  + "</td><td>" + minutes + "</td><td>"+ remove +  "</td></tr>");
   
-  // When document loads
-  $(document).ready(()=> {
-  
-  
-  
-  
-      // on sign in submit 
-      $('#sign-in-submit').on('click', (e) => {
-  
-          e.preventDefault()
-  
-          // Grab user inputs
-          let userEmail = $('#email-input').val().trim(),
-              userPassword = $('#password-input').val().trim()
-      
-          console.log(userEmail);
-          console.log(userPassword);
-  
-          firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // [START_EXCLUDE]
-              if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-              } else {
-                alert(errorMessage);
-              }
-              console.log(error);
-              // [END_EXCLUDE]
-          });
-  
-          firebase.auth().onAuthStateChanged(function(user) {
-              if (user) {
-                // User is signed in.
-                console.log(user);
-                
-              } else {
-                // No user is signed in.
-                console.log('Not signed in');
-                
-              }
-          });
-  
-          $('#email-input').val('')
-          $('#password-input').val('')
+  }, function(err) {
+        console.log(err);
+    });
+
+    
+
+    
+        $(document).on("click", ".glyphicon-trash", deleteTrain);
+
+         function deleteTrain() {
+        	alert("are you sure , you want delete this data ??");
           
-      })
+        	var deleteKey = $(this).attr("id");
+            database.ref().child(deleteKey).remove();
+            location.reload();
+        }
   
+   });   
 
-      $('#sign-up-submit').on('click', (e) => {
-  
-          e.preventDefault()
-  
-          let userName = $('#user-name-input').val().trim(),
-              userEmail = $('#user-email-input').val().trim(),
-              userPassword = $('#user-password-input').val().trim(),
-              userPasswordConfirm = $('#user-passwordConfirm-input').val().trim()
-              
-              
-              firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-                  // Handle Errors here.
-                  var errorCode = error.code;
-                  var errorMessage = error.message;
-                  // ...
-              });
-              
-              $('#user-name-input').val('')
-              $('#user-email-input').val('')
-              $('#user-password-input').val('')
-              $('#user-passwordConfirm-input').val('')
-  
-      })
-  
-  
-  
-  
-  
-  
-  
-      
 
-      $('#train-submit').on('click', (e) => {
-  
-          
-              e.preventDefault()
-              
-              // User inputs except for minutesAway & nextArrival
-              var name = $('#train-name').val().trim(),
-                  destination = $('#train-destination').val().trim(),
-                  frequency = $('#train-frequency').val().trim(),
-                  time = $('#train-time').val().trim()
-  
-              // Conversions for nextArrival & minutesAway
-              // Assumptions
-              var runFrequency = frequency,
-                  // First train runs @
-                  trainStartTime = time,
-                  // Take the irst time and pushes the date back a year to make sure it comes before the current time
-                  backOneYear = moment(trainStartTime, 'HH:mm').subtract(1, 'years'),
-                  // Current time
-                  currentTime = moment(),
-                  // Difference between the times 
-                  diffInMinutes = moment().diff(moment(backOneYear), "minutes"),
-                  // Time appart (remainder)
-                  tRemainder = diffInMinutes % runFrequency,
-                  // Minutes until train 
-                  minutesTillNextTrain = runFrequency - tRemainder,
-                  // Next train
-                  nextTrainArrival = moment().add(minutesTillNextTrain, 'minutes')
-                  
-                  trainDataRef.push({
-                      name: name,
-                      destination: destination,
-                      time: time,
-                      frequency: frequency,
-                      nxtArrival: moment(nextTrainArrival).format('hh:mm A'),
-                      minsAway: minutesTillNextTrain
-                  })
-                  
-                  
-                  $('#train-name').val('')
-                  $('#train-destination').val('')
-                  $('#train-frequency').val('')
-                  $('#train-time').val('')
-                  
-                  
-              
-              })
-          })
-  
-          
-  trainDataRef.on('child_added', function(snap){
-      if(snap){
-          $('#train-table tbody').append(buildTrain(snap.val().name, snap.val().destination, snap.val().frequency, snap.val().nxtArrival, snap.val().minsAway))
-      }
-  }, function(errorObject){
-      console.log('Errors handled: ' + errorObject.code)
-  })
-  
-  
+	
 
-  trainDataRef.orderByChild('dateAdded').limitToLast(1).on('child_added', function(snap){
-      if(snap){
-          $('#recent-train-table tbody').html(buildTrain(snap.val().name, snap.val().destination, snap.val().frequency, snap.val().nxtArrival, snap.val().minsAway))
-      }
-  }, function(errorObject){
-      console.log('Erros handled: ' + errorObject.code);
-      
-  })
-  
 
-  
-  function buildTrain(name, destination, frequency, nxtArrival, minsAway){
-      
-      return $(
-          '<tr>'+
-          '<th class="t-name">'+ name +'</th>'+
-              '<td class="t-destination">'+ destination +'</td>'+
-              '<td class="t-frequency">'+ frequency +'</td>'+
-              '<td class="t-nxtArrival">'+ nxtArrival +'</td>'+
-              '<td class="t-minsAway">'+ minsAway +'</td>'+
-              '</tr>'
-          )
-      }
-      
-  
-  
-  
